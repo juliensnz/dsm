2bd2ed4d7d227cabdbb1844795a1e0b60a1217fc
juliensnz <contact@akeneo.com>
2020-08-11 21:26:33 +0000

    Update from https://github.com/juliensnz/pim-community-dev/commit/5f6072cd2a9d1fd58411d2f9505a4198ae6230ce

--- lib/components/Buttons/CoreButton/CoreButton.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-declare const CoreButton: React.ForwardRefExoticComponent<{
-    ariaLabel?: string | undefined;
-    ariaLabelledBy?: string | undefined;
-    ariaDescribedBy?: string | undefined;
-    children: ReactNode;
-    sizeMode?: "small" | "large" | undefined;
-} & React.ButtonHTMLAttributes<HTMLButtonElement> & React.RefAttributes<HTMLButtonElement>>;
-export { CoreButton };
